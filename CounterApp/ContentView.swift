1e1f3e2e5dfb0b69e7c7196af46331968d4d3bd3
Dzhafar Nasirov <xjfxnsx@gmail.com>
2025-03-15 22:18:09 +0400

    added buttons

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -12,12 +12,41 @@ struct ContentView: View {
     @State private var count = 0
     
     var body: some View {
-            VStack {
-                Text("Counter: \(count)")  // Выводим текущее значение
-                    .font(.largeTitle)
-                    .padding()
+        
+        VStack {
+            
+            Text("Counter: \(count)")  // Выводим текущее значение
+                .font(.largeTitle)
+                .padding()
+            
+            HStack {
+                
+                Button(action: {
+                    count -= 1
+                }) {
+                    Text("-")
+                        .font(.largeTitle)
+                        .frame(width: 80, height: 80)
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
+                Button(action: {
+                    count += 1
+                }) {
+                    Text("+")
+                        .font(.largeTitle)
+                        .frame(width: 80, height: 80)
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+                
             }
+            .padding()
         }
+    }
 }
 
 #Preview {
